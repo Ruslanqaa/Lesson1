9eb8e7ee0b2e45a24070e846053112f476239ff3
Ruslanqaa <ruskiseljov1@gmail.com>
2024-07-30 21:00:45 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+#Задание 1
+number_of_homework_completed = 12
+number_of_hours_spent = 1.5
+cours_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_homework_completed
+print("Курс: " + cours_name + ",""Всего задач:",number_of_homework_completed,", Затрачено часов:", number_of_hours_spent,"среднее время выполнения", time_for_one_task)
+
+
+
